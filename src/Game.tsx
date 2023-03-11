f3f6c8f3cf3a956687c7241bf8504b927ed369e1
kiseki-kiseki <kcanggua@gmail.com>
2023-03-12 04:57:57 +0900

    clickしたら止まるようにした

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { css } from "@emotion/react";
+import { useState, useEffect } from "react";
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-
-const Game = () => {
+import bottom from "./images/bottom.png"
+import top from "./images/top.png"
+import { setInterval } from "timers/promises";
   const all = css`
     width: 100vw;
     height: 100vh;
@@ -22,12 +24,49 @@ const Game = () => {
     background-color: red;
     border-radius: 20px;
     margin-top: 30px;
+    display: flex;
+    flex-flow: column;
+    justify-content:center;
   `
-  const navigate = useNavigate();
+  const bottomImage = css`
+    width: 100%;
+    height: 300px;
+  `
+const Game = () => {
+  const [left, setLeft] = useState<number>(0);
+  const [isClicked, setIsClicked] = useState<boolean>(false)
+  const changeIsClicked = () => {
+    setIsClicked((prev) => !prev)
+  }
+  useEffect(() => {
+    const changeImagePosition = window.setInterval(() => {
+      if(isClicked) {
+        return
+      } else {
+        console.log(isClicked);
+        setLeft((prev) => (prev % 320) + 1)
+      }
+    }, 1);
+    return () => window.clearInterval(changeImagePosition);
+  }, [isClicked]);
+
+  const topImage = (left: number) => css`
+    position: absolute;
+    width: 270px;
+    height: 160px;
+    left: ${left + 480}px
+  `
   return (
     <div css={all}>
-      <div css={title}>Pitatto</div>
-      <div css={game}></div>
+      <div css={title} onClick={() => {
+        changeIsClicked()
+        console.log(isClicked);
+        
+      }}>Pitatto</div>
+      <div css={game} onClick={changeIsClicked}>
+        <img src={bottom} alt="bottom" css={bottomImage} />
+        <img src={top} alt="top" css={topImage(left)} />
+      </div>
     </div>
   )
 }
