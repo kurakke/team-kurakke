b65d7cdd3e581615436ae3bbde3e5fd5da8a4f2b
kiseki-kiseki <kcanggua@gmail.com>
2023-03-11 23:13:31 +0900

    一応resetcss入れた

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,13 +4,30 @@ import { css } from "@emotion/react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 const Game = () => {
-  const bgcolor = css`
-    background-color:red;
+  const all = css`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-flow: column;
+  `
+  const title = css`
+    margin-top: 30px;
+    font-weight: 800;
+    font-size: 40px;
+  `
+  const game = css`
+    width: 550px;
+    height: 500px;
+    background-color: red;
+    border-radius: 20px;
+    margin-top: 30px;
   `
   const navigate = useNavigate();
   return (
-    <div>
-      <h2 css={bgcolor}>GamePage</h2>
+    <div css={all}>
+      <div css={title}>Pitatto</div>
+      <div css={game}></div>
     </div>
   )
 }
