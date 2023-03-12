8ed5c140edd23c52b104384e3ce4ef5807d81cfa
kiseki-kiseki <kcanggua@gmail.com>
2023-03-12 11:23:07 +0900

    api一旦諦め

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,6 +7,13 @@ import bottom from "./images/bottom.png"
 import top from "./images/top.png"
 import { useCallback } from "react";
 import axios from 'axios';
+import { Module } from "module";
+  // type message = {
+  //   message: string
+  // }
+  // interface HTMLButtonEvent extends Event {
+  //   target: HTMLButtonElement;
+  // }
   const all = css`
     width: 100vw;
     height: 100vh;
@@ -33,7 +40,7 @@ import axios from 'axios';
     width: 100%;
     height: 300px;
   `
-  const API_KEY = "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"
+  // const API_KEY = "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"
 const Game = () => {
   const [left, setLeft] = useState<number>(0);
   const [isClicked, setIsClicked] = useState<boolean>(false)
@@ -65,18 +72,62 @@ const Game = () => {
     height: 160px;
     left: ${left + 480}px
   `
+  // const API_URL = ""
+  // const MODEL = ""
+  // const chat = async ( message:message ) => {
+  //   try {
+  //     const response = await axios.post( `${ API_URL }chat/completions`, {
+  //       // モデル ID の指定
+  //       model: MODEL,
+  //       // 質問内容
+  //       messages: [
+  //         {
+  //           'role': 'user',
+  //           'content': message,
+  //         }
+  //       ],
+  //     }, {
+  //       // 送信する HTTP ヘッダー(認証情報)
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Authorization': `Bearer ${ API_KEY }`
+  //       }
+  //     });
+  //     // 回答の取得
+  //     return response.data.choices[0].message.content;
+   
+  //   } catch ( error ) {
+  //     console.error( error );
+  //     return null;
+  //   }
+  // }
+  // const [message, setMessage] = useState<message>("")
+  // const [answer, setAnswer] = useState('')
+  // const handleMessageChange = ( event:  )  => {
+  //   setMessage( event.target.value );
+  // }
+  // const handleSubmit = async ( event: HTMLButtonEvent ) => {
+  //   event.preventDefault();
+ 
+  //   // chat.js にメッセージを渡して API から回答を取得
+  //   const responseText = await chat( message: message );
+ 
+  //   // 回答の格納
+  //   setAnswer( responseText );
+  // }
   return (
-    <div css={all}>
+    <><div css={all}>
       <div css={title} onClick={() => {
-        changeIsClicked()
+        changeIsClicked();
         console.log(isClicked);
-        
-      }}>Pitatto</div>
-      <div css={game} onClick={changeIsClicked}>
+
+      } }>Pitatto</div>
+
+    <div css={game} onClick={changeIsClicked}>
         <img src={bottom} alt="bottom" css={bottomImage} />
         <img src={top} alt="top" css={topImage(left)} />
       </div>
-    </div>
+    </div></>
   )
 }
 
